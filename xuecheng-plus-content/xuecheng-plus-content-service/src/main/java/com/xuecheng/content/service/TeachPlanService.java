297acab52993592700890e4b8af84e43fe286ab2
JiaYF98 <embrace-@outlook.com>
2024-08-19 23:36:43 +0800

    实现查询课程计划功能

--- xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/TeachPlanService.java
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/TeachPlanService.java
@@ -1,11 +1,9 @@
 package com.xuecheng.content.service;
 
 import com.xuecheng.content.model.dto.TeachPlanTreeDTO;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
 public interface TeachPlanService {
     List<TeachPlanTreeDTO> getTeachPlanTreeNodes(Long courseId);
 }
